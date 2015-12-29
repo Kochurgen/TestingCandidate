22e75485dc6d2982458be6c1aa5d4e946012bb6f
vladimir <v.kochubenko@mobidev.biz>
2015-12-29 11:02:53 +0200

    admin panel

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,21 @@
-/**
- * Created by vladimir on 12/29/15.
- */
+url = "localhost:3000";
+console.log('connect');
+$('#getTests').on('click',getTestsList());
+$('#getUsers').on('click',getUsers());
+
+function getUsers(){
+    $.ajax({
+        url: url+"/account/getUsers",
+        context: document.body
+    }).done(function() {
+
+    });
+}
+function getTestsList(){
+    $.ajax({
+        url: url+"/account/getTestlist",
+        context: document.body
+    }).done(function() {
+
+    });
+}
